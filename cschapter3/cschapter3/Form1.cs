dc3d431667c408b4aa22925255859913b72bbb57
jinwon_Hyun <52765408+jinwonHyun@users.noreply.github.com>
2020-10-13 21:24:47 +0900

    Chapter 3_1

---
+++ cschapter3/cschapter3/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cschapter3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int len = talker.BlahBlahBlah(textBox1.Text, (int)numericUpDown1.Value);
+            MessageBox.Show("The mmessage length is " + len);//줄바꿈 문자도 length에 포함됨.
+        }
+    }
+}
